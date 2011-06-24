1ee88151a6f832df07d737bf270f9aadc46805bf
xNidhogg <nidhogg1@gmx.de>
2011-06-24 10:53:00 +0200

    Created Repo

---
+++ BlackDragon/BlackDragon/Game1.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Tile_Engine;
+
+namespace Gemstone_Hunter
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+        SpriteFont pericles8;
+        Vector2 scorePosition = new Vector2(20, 580);
+        enum GameState { TitleScreen, Playing, PlayerDead, GameOver };
+        GameState gameState = GameState.TitleScreen;
+
+        Vector2 gameOverPosition = new Vector2(350, 300);
+        Vector2 livesPosition = new Vector2(600, 580);
+
+        Texture2D titleScreen;
+
+        float deathTimer = 0.0f;
+        float deathDelay = 5.0f;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            TileMap.Initialize(Content.Load<Texture2D>(@"Textures/PlatformTiles"));
+            pericles8 = Content.Load<SpriteFont>(@"fonts/pericles8");
+            titleScreen = Content.Load<Texture2D>(@"textures/TitleScreen");
+
+            Camera.WorldRectangle = new Rectangle(0, 0, 160 * 48, 12 * 48);
+            Camera.Position = Vector2.Zero;
+            Camera.ViewPortWidth = 800;
+            Camera.ViewPortHeight = 600;
+
+            player = new Player(Content);
+            LevelManager.Initialize(Content, player);    
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            KeyboardState keyState = Keyboard.GetState();
+            GamePadState padState = GamePad.GetState(PlayerIndex.One);
+
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            switch (gameState) { 
+                case GameState.TitleScreen:
+                    if (keyState.IsKeyDown(Keys.Enter) || padState.IsButtonDown(Buttons.A)) {
+                        StartNewGame();
+                        gameState = GameState.Playing;
+                    }
+                    break;
+                case GameState.Playing:
+                    player.Update(gameTime);
+                    LevelManager.Update(gameTime);
+                    if (player.Dead) {
+                        if (player.LivesRemaining > 0)
+                        {
+                            gameState = GameState.PlayerDead;
+                            deathTimer = 0.0f;
+                        }
+                        else {
+                            gameState = GameState.GameOver;
+                            deathTimer = 0.0f;
+                        }
+                    }
+                    break;
+                case GameState.PlayerDead:
+                    player.Update(gameTime);
+                    deathTimer += elapsed;
+                    if (deathTimer > deathDelay) {
+                        player.WorldLocation = Vector2.Zero;
+                        LevelManager.ReloadLevel();
+                        player.Revive();
+                        gameState = GameState.Playing;
+                    }
+                    break;
+                case GameState.GameOver:
+                    deathTimer += elapsed;
+                    if (deathTimer > deathDelay) {
+                        gameState = GameState.TitleScreen;
+                    }
+                    break;
+            }
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+            if (gameState == GameState.TitleScreen) {
+                spriteBatch.Draw(titleScreen, Vector2.Zero, Color.White);
+            }
+            else {
+                TileMap.Draw(spriteBatch);
+                player.Draw(spriteBatch);
+                LevelManager.Draw(spriteBatch);
+                spriteBatch.DrawString(pericles8, "Score: " + player.Score.ToString(), scorePosition, Color.White);
+                spriteBatch.DrawString(pericles8, "Lives Remaining: " + player.LivesRemaining.ToString(), livesPosition, Color.White);
+            }
+            if (gameState == GameState.GameOver) {
+                spriteBatch.DrawString(pericles8, "G A M E  O V E R !", gameOverPosition, Color.White);
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void StartNewGame() {
+            player.Revive();
+            player.LivesRemaining = 3;
+            player.WorldLocation = Vector2.Zero;
+            LevelManager.LoadLevel(0);
+        }
+    }
+}
---
+++ Tile Engine/TileMap.cs
@@ -0,0 +1,216 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Tile_Engine
+{
+    public static class TileMap
+    {
+        #region Declarations
+        public const int TileWidth = 32;
+        public const int TileHeight = 32;
+        public const int MapWidth = 128;
+        public const int MapHeight = 12;
+        public const int MapLayers = 3;
+        private const int skyTile = 2;
+
+        static private MapSquare[,] mapCells = new MapSquare[MapWidth, MapHeight];
+
+        public static bool EditorMode = false;
+
+        public static SpriteFont spriteFont;
+        private static Texture2D tileSheet;
+        #endregion
+
+        #region Initialization
+        public static void Initialize(Texture2D tileTexture) {
+            tileSheet = tileTexture;
+            for (int x = 0; x < MapWidth; ++x) {
+                for (int y = 0; y < MapHeight; ++y) {
+                    for (int z = 0; z < MapLayers; ++z) {
+                        mapCells[x, y] = new MapSquare(skyTile, 0, 0, "", true);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Tile and Tile Sheet Handling
+        public static int TilesPerRow {
+            get { return tileSheet.Width / TileWidth; }
+        }
+
+        public static Rectangle TileSourceRectangle(int tileIndex) {
+            return new Rectangle((tileIndex % TilesPerRow) * TileWidth, (tileIndex / TilesPerRow) * TileHeight, TileWidth, TileHeight);
+        }
+        #endregion
+
+        #region Information about Map Cells
+        public static int GetCellByPixelX(int pixelX) {
+            return pixelX / TileWidth;
+        }
+
+        public static int GetCellByPixelY(int pixelY) {
+            return pixelY / TileHeight;
+        }
+
+        public static Vector2 GetCellByPixel(Vector2 pixelLocation) {
+            return new Vector2(GetCellByPixelX((int)pixelLocation.X), GetCellByPixelY((int)pixelLocation.Y));
+        }
+
+        public static Vector2 GetCellCenter(int cellX, int cellY) { 
+            return new Vector2((cellX * TileWidth) + (TileWidth /2), (cellY * TileHeight) + (TileHeight /2));
+        }
+
+        public static Vector2 GetCellCenter(Vector2 cell) {
+            return GetCellCenter((int)cell.X, (int)cell.Y);
+        }
+
+        public static Rectangle CellWorldRectangle(int cellX, int cellY) {
+            return new Rectangle(cellX * TileWidth, cellY * TileHeight, TileWidth, TileHeight);
+        }
+
+        public static Rectangle CellWorldRectangle(Vector2 cell) {
+            return CellWorldRectangle((int)cell.X, (int)cell.Y);
+        }
+
+        public static Rectangle CellScreenRectangle(int cellX, int cellY) {
+            return Camera.WorldToScreen(CellWorldRectangle(cellX, cellY));
+        }
+
+        public static Rectangle CellScreenRectangle(Vector2 cell) {
+            return CellScreenRectangle((int)cell.X, (int)cell.Y);
+        }
+
+        public static bool CellIsPassable(int cellX, int cellY) {
+            MapSquare square = GetMapSquareAtCell(cellX, cellY);
+            if (square == null) return false;
+            else return square.Passable;
+        }
+
+        public static bool CellIsPassable(Vector2 cell) {
+            return CellIsPassable((int)cell.X, (int)cell.Y);
+        }
+
+        public static bool CellIsPassableByPixel(Vector2 pixelLocation) { 
+            return CellIsPassable(GetCellByPixelX((int)pixelLocation.X), GetCellByPixelY((int)pixelLocation.Y));
+        }
+
+        public static string CellCodeValue(int cellX, int cellY) {
+            MapSquare square = GetMapSquareAtCell(cellX, cellY);
+
+            if (square == null) return "";
+            else return square.CodeValue;
+        }
+
+        public static string CellCodeValue(Vector2 cell) {
+            return CellCodeValue((int)cell.X, (int)cell.Y);
+        }
+        #endregion
+
+        #region Information about MapSquare objects
+        public static MapSquare GetMapSquareAtCell(int tileX, int tileY) {
+            if ((tileX >= 0) && (tileX < MapWidth) && (tileY >= 0) && (tileY < MapHeight)) {
+                return mapCells[tileX, tileY];
+            }
+            else return null;
+        }
+
+        public static void SetMapSquareAtCell(int tileX, int tileY, MapSquare tile) {
+            if ((tileX >= 0) && (tileX < MapWidth) && (tileY >= 0) && (tileY < MapHeight)) {
+                mapCells[tileX, tileY] = tile;
+            }
+        }
+
+        public static void SetTileAtCell(int tileX, int tileY, int layer, int tileIndex) {
+            if ((tileX >= 0) && (tileX < MapWidth) && (tileY >= 0) && (tileY < MapHeight)) {
+                mapCells[tileX, tileY].LayerTiles[layer] = tileIndex;
+            }
+        }
+
+        public static MapSquare GetMapSquareAtPixel(int pixelX, int pixelY) {
+            return GetMapSquareAtCell(GetCellByPixelX(pixelX), GetCellByPixelY(pixelY));
+        }
+
+        public static MapSquare GetMapSquareAtPixel(Vector2 pixelLocation) {
+            return GetMapSquareAtPixel((int)pixelLocation.X, (int)pixelLocation.Y);
+        }
+        #endregion
+
+        #region Drawing
+        public static void Draw(SpriteBatch spriteBatch) { 
+            int startX = GetCellByPixelX((int)Camera.Position.X);
+            int endX = GetCellByPixelX((int)Camera.Position.X + Camera.ViewPortWidth);
+
+            int startY = GetCellByPixelY((int)Camera.Position.Y);
+            int endY = GetCellByPixelY((int)Camera.Position.Y + Camera.ViewPortHeight);
+
+            for (int x = startX; x <= endX; ++x) {
+                for (int y = startY; y <= endY; ++y) {
+                    for (int z = 0; z < MapLayers; ++z) {
+                        if ((x >= 0) && (y >= 0) && (x < MapWidth) && (y < MapHeight)) {
+                            spriteBatch.Draw(tileSheet, CellScreenRectangle(x, y), TileSourceRectangle(mapCells[x, y].LayerTiles[z]), Color.White, 0.0f, Vector2.Zero, SpriteEffects.None, 1f - ((float)z * 0.1f));
+                        }
+                    }
+                    if (EditorMode) {
+                        DrawEditModeItems(spriteBatch, x, y);
+                    }
+                }
+            }
+        }
+
+        public static void DrawEditModeItems(SpriteBatch spriteBatch, int x, int y) {
+            if ((x < 0) || (y < 0) || (x >= MapWidth) || (y >= MapHeight)){
+                return;
+            }
+            if (!CellIsPassable(x, y)) {
+                spriteBatch.Draw(tileSheet, CellScreenRectangle(x, y), TileSourceRectangle(1), new Color(255, 0, 0, 80), 0f, Vector2.Zero, SpriteEffects.None, 0f);
+            }
+            if (mapCells[x, y].CodeValue != "") {
+                Rectangle screenRect = CellScreenRectangle(x, y);
+
+                spriteBatch.DrawString(spriteFont, mapCells[x, y].CodeValue, new Vector2(screenRect.X, screenRect.Y), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            }
+        }
+        #endregion
+
+        #region Loading and Saving
+        public static void SaveMap(FileStream fileStream) {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, mapCells);
+            fileStream.Close();
+        }
+
+        public static void LoadMap(FileStream fileStream) {
+            try {
+                BinaryFormatter formatter = new BinaryFormatter();
+                mapCells = (MapSquare[,])formatter.Deserialize(fileStream);
+                fileStream.Close();
+            } catch {
+                ClearMap();
+            }
+        }
+
+        public static void ClearMap() {
+            for (int x = 0; x < MapWidth; ++x) {
+                for (int y = 0; y < MapHeight; ++y) {
+                    for (int z = 0; z < MapLayers; ++z) {
+                        mapCells[x, y] = new MapSquare(skyTile, 0, 0, "", true);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
