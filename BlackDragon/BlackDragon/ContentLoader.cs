0baf826af097a208f9e02758c79fdc0a3461f012
xNidhogg <nidhogg1@gmx.de>
2011-07-14 15:42:21 +0200

    Added files to repo I forgot last Time.

---
+++ BlackDragon/BlackDragon/ContentLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Tile_Engine;
+using BlackDragon.Managers;
+using BlackDragon.Providers;
+
+namespace BlackDragon
+{
+    static class ContentLoader
+    {
+        public static void LoadContent(ContentManager Content)
+        {  
+            FontProvider.AddFont("Pericles8", Content.Load<SpriteFont>(@"fonts/pericles8"));
+            FontProvider.AddFont("Pericles9", Content.Load<SpriteFont>(@"fonts/pericles9")); 
+            FontProvider.AddFont("Pericles14", Content.Load<SpriteFont>(@"fonts/pericles14"));
+
+            PlatformManager.LoadContent(Content.Load<Texture2D>(@"textures/TileSets/platform"));
+            RPGManager.LoadContent(Content.Load<Texture2D>(@"textures/TileSets/rpg"));
+
+            TitleScreenManager.LoadContent(Content.Load<Texture2D>(@"textures/misc/TitleScreen"));           
+        }
+    }
+}
---
+++ BlackDragon/BlackDragon/Controller/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using BlackDragon.Menus;
+using BlackDragon.Providers;
+
+namespace BlackDragon.Controller
+{
+    static class MenuController
+    {
+        public static void Update(Menu menu)
+        {
+            if (ShortcutProvider.KeyPressedNowButNotLastFrame(Keys.S) || ShortcutProvider.KeyPressedNowButNotLastFrame(Keys.Down))
+            {
+                menu.NextMenuItem();
+            }
+
+            if (ShortcutProvider.KeyPressedNowButNotLastFrame(Keys.W) || ShortcutProvider.KeyPressedNowButNotLastFrame(Keys.Up))
+            {
+                menu.PreviousMenuItem();
+            }
+
+            menu.ResolveMouseSelection();
+
+            menu.UpdateColors();
+
+            if (ShortcutProvider.KeyPressedNowButNotLastFrame(Keys.Enter) || ShortcutProvider.LeftButtonClickedNowButNotLastFrame())
+            {
+                menu.SelectMenuItem();
+            }
+        }
+    }
+}
