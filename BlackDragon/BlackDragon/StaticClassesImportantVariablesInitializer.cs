00529a14da4a6f4d54b5f4e2e43524be699c632e
xNidhogg <nidhogg1@gmx.de>
2011-07-14 15:55:31 +0200

    fixed the stupid ignore file

---
+++ BlackDragon/BlackDragon/StaticClassesImportantVariablesInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Tile_Engine;
+
+namespace BlackDragon
+{
+    static class StaticClassesImportantVariablesInitializer
+    {
+        public static void InitializeImportantVariables()
+        {            
+            Camera.Position = Vector2.Zero;
+            Camera.ViewPortWidth = 800;
+            Camera.ViewPortHeight = 600;
+        }
+    }
+}
